58bdb0257f4ef5581d25796d33d9a44a5e80566f
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-07-31 15:48:42 -0300

    chore: finishing ex02

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:35:09 by marianasena       #+#    #+#             */
+/*   Updated: 2025/07/31 15:48:20 by marianasena      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
