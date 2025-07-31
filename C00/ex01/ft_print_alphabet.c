34b8d0ba5fe233612eeaa82bad9be57514bdd1ab
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-07-31 15:38:43 -0300

    fix: ft_print_alphabet norm fixes

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,14 +6,16 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 19:48:14 by marianasena       #+#    #+#             */
-/*   Updated: 2025/07/31 15:34:05 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/07/31 15:38:20 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_print_alphabet(void)
 {
-	int i = 97;
+	int	i;
+
+	i = 97;
 	while (i <= 122)
 	{
 		write(1, &i, 1);
