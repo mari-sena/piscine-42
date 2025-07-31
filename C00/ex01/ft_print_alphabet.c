ba1f57e773cb6a075bb51761bbc88728f398f740
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-07-31 15:34:40 -0300

    chore: ex01

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,22 +6,23 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 19:48:14 by marianasena       #+#    #+#             */
-/*   Updated: 2025/07/31 09:51:48 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/07/31 15:34:05 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
 void	ft_print_alphabet(void)
 {
 	int i = 97;
 	while (i <= 122)
 	{
-		write(1, &d, 1);
+		write(1, &i, 1);
 		i++;
 	}
 }
 
-int	main()
+/*int	main()
 {
 	ft_print_alphabet();
 	return(0);
-}
+}*/
