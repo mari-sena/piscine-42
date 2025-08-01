f37a36781d7366b7c5f3f769b1502b8baac6bed0
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-08-01 10:24:42 -0300

    chore: finishing ultimate_ft

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 17:53:36 by marianasena       #+#    #+#             */
+/*   Updated: 2025/07/31 21:38:28 by marianasena      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	aux;
+	int	*pointer;
+	int	**pointer2;
+	int	***pointer3;
+	int	****pointer4;
+	int	*****pointer5;
+	int	******pointer6;
+	int	*******pointer7;
+	int	********pointer8;
+	int	*********nbr;
+
+	aux = 0;
+	pointer = &aux;
+	pointer2 = &pointer;
+	pointer3 = &pointer2;
+	pointer4 = &pointer3;
+	pointer5 = &pointer4;
+	pointer6 = &pointer5;
+	pointer7 = &pointer6;
+	pointer8 = &pointer7;
+	nbr = &pointer8;
+
+	ft_ultimate_ft(nbr);
+	printf("%d", *********nbr);
+	return(0);
+}*/
