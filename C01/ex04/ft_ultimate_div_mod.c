f33de02e87efcd3fce959de12245ee92c9a2cae5
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-08-03 16:20:17 -0300

    fix: ft ultimate div mod

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -6,18 +6,29 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 19:49:47 by marianasena       #+#    #+#             */
-/*   Updated: 2025/08/01 19:50:49 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/08/03 16:19:53 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
+	int	aux;
 
+	aux = *a;
+	*a = *a / *b;
+	*b = aux % *b;
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 int	main(void)
 {
-	ft_ultimate_div_mod();
+	int	a;
+	int	b;
+
+	a = 4;
+	b = 8;
+	ft_ultimate_div_mod(&a, &b);
+	printf("a: %d\n", a);
+	printf("b: %d\n", b);
 	return(0);
-}
+}*/
