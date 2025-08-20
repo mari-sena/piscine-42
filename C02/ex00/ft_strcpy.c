49e2403277f43f2ca933d7f467c1b695a6f3345b
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-08-20 14:54:46 -0300

    chore: finishing exercise ft_strcpy

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 18:47:33 by marianasena       #+#    #+#             */
+/*   Updated: 2025/08/20 14:53:51 by marianasena      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	char	nome[8] = "Mariano";
+
+	printf("%s", ft_strcpy(nome, "Mariana"));
+	return (0);
+}*/
