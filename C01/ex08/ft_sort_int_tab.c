573dc3939ecce0dafae705894f5da85428883265
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-09-05 19:50:44 -0300

    continuing ft sort int tab

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,11 +6,27 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 19:48:20 by marianasena       #+#    #+#             */
-/*   Updated: 2025/09/04 19:48:52 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/09/04 20:41:21 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	i;
 
+	i = size;
+	j = 0;
+	while (i <= 0)
+	{
+		
+		i++;
+	}
+}
+
+int	main(void)
+{
+	int	intArr = [1, 2, 3];
+
+	ft_sort_int_tab(intArr, 3);
+	return (0);
 }
