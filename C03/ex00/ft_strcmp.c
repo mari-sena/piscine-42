c4d273f3575594b5e26ff69636c3dd816b9a7ed7
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-09-10 20:56:43 -0300

    tentando entender o strcmp

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,11 +6,22 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:46:15 by marianasena       #+#    #+#             */
-/*   Updated: 2025/09/09 19:37:04 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/09/10 20:56:22 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
+	return (0);
+}
 
+#include <string.h>
+#include <stdio.h>
+int	main(void)
+{
+	char	s1[6] = "maria";
+	char	s2[6] = "mario";
+
+	printf("%d", strcmp("56", "123"));
+	return (0);
 }
