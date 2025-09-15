2a15544c7cf75beb5d1e8a3ae50781047aa8cfcc
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-09-15 20:59:33 -0300

    building ft_strcmp test

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,12 +6,19 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:46:15 by marianasena       #+#    #+#             */
-/*   Updated: 2025/09/10 20:56:22 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/09/15 20:59:09 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
+	int	aux;
+
+	aux = 0;
+	while (s1[aux] != '\0')
+	{
+	
+	}
 	return (0);
 }
 
@@ -19,9 +26,11 @@ int	ft_strcmp(char *s1, char *s2)
 #include <stdio.h>
 int	main(void)
 {
-	char	s1[6] = "maria";
-	char	s2[6] = "mario";
+	char	s1[6] = "maraa";
+	char	s2[6] = "marpo";
 
-	printf("%d", strcmp("56", "123"));
+	printf("Result: %d , Expected: -15\n", ft_strcmp("maraa", "marpo"));
+	printf("Result: %d , Expected: -14", ft_strcmp("maria", "mario"));
+	printf("%d", strcmp(s1, s2));
 	return (0);
 }
