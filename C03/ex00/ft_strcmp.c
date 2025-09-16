b89e317ee581214118702b9b961b3a2d69b65bb3
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-09-16 20:09:39 -0300

    strcmp

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,9 +6,10 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:46:15 by marianasena       #+#    #+#             */
-/*   Updated: 2025/09/15 21:05:36 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/09/15 21:14:28 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -22,6 +23,7 @@ int	ft_strcmp(char *s1, char *s2)
 		int	temp;
 
 		temp =  s1[aux] - s2[aux];
+		printf("%d\n", temp);
 		result += temp;
 		aux++;
 	}
@@ -29,16 +31,16 @@ int	ft_strcmp(char *s1, char *s2)
 }
 
 #include <string.h>
-#include <stdio.h>
+//#include <stdio.h>
 int	main(void)
 {
 	char	s1[6] = "maraa";
 	char	s2[6] = "marpo";
-	char	s3[6] = "maria";
-	char	s4[6] = "mario";
+	char	s3[6] = "mari";
+	char	s4[6] = "mari";
 
-	printf("Result: %d , Expected: -15\n", ft_strcmp(s1, s2));
-	printf("Result: %d , Expected: -14✅\n", ft_strcmp(s3, s4));
+	printf("Result: %d | Expected: -15\n", ft_strcmp(s1, s2));
+	//printf("Result: %d | Expected: -14 ✅\n", ft_strcmp(s3, s4));
 	//printf("%d", strcmp(s1, s2));
 	return (0);
 }
