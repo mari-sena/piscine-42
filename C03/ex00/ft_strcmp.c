efe29de3cb05f94eb0696a16572064ba138b6e81
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-09-24 19:32:55 -0300

    arrumei 2 cenários novos e falta o último - comparacao de pponteiro

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,47 +6,44 @@
 /*   By: marianasena <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:46:15 by marianasena       #+#    #+#             */
-/*   Updated: 2025/09/24 14:40:40 by marianasena      ###   ########.fr       */
+/*   Updated: 2025/09/24 19:30:33 by marianasena      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strsize(char str)
-{
-	str
-}
+#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	aux;
 	int	result;
-	int	s1size;
-	int	s2size;
 
 	aux = 0;
 	result = 0;
-	s1size = ft_strsize(s1);
-	s2size = ft_strsize(s2);
-	while (s1[aux] != '\0' && result == 0)
+	while (s1[aux] != '\0' && s2[aux] != '\0'  && result == 0)
 	{
 		result =  s1[aux] - s2[aux];
+		printf('%d', &s1[aux]);
+		printf('%d', &s2[aux]);
 		aux++;
 	}
-	if (s2[aux] != '\0')
-		return (-s2[aux]);
+	if (s2[aux] != '\0') return (-s2[aux]);
+	if (s1[aux] != '\0') return (s1[aux]);
 	return (result);
 }
 
-/*#include <string.h>
-#include <stdio.h>
+#include <string.h>
+//#include <stdio.h>
+
 int	main(void)
 {
 	char	s1[6] = "maraa";
 	char	s2[6] = "marpo";
-	char	s3[6] = "mari";
-	char	s4[6] = "mari";
+	char	s3[5] = "mari";
+	char	s4[5] = "mari";
+	char	dupTest[6] = "World";
 
-	printf("Result: %d | Expected: -15\n", ft_strcmp(s1, s2));
+	printf("%s", strdup(dupTest));
+	//printf("Result: %d | Expected: -15\n", ft_strcmp(s1, s2));
 	//printf("Result: %d | Expected: -14 ✅\n", ft_strcmp(s3, s4));
 	//printf("%d", strcmp(s1, s2));
 	return (0);
-}*/
+}
