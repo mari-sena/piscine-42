ab3685c75c303b51e0ca1424928495ca57813098
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2026-01-04 21:35:10 -0300

    trying

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -10,31 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcat(char *dest, char *src)
-{
-	char*	result = '';
-	int		aux = 0;
 
-	while (dest[aux] != '\0') {
-		aux++;
-		result += dest[aux];
-	}
-	aux = 0;
-	while (src[aux] != '\0') {
-		aux++;
-		result += src[aux];
-	}
-	return (result);
-}
+// char	*ft_strcat(char *dest, char *src)
+// {
+// 	char*	result[1] = 's';
+// 	int		aux = 0;
+// 	int		result_aux = 0;
+	
+// 	while (dest[aux] != '\0') {
+// 		result[aux] = &dest[aux];
+// 		aux++;
+// 		result_aux++;
+// 		printf("%s", result[result_aux]);
+// 	}
+// 	// aux = 0;
+// 	// while (src[aux] != '\0') {
+// 	// 	aux++;
+// 	// 	result += src[aux];
+// 	// }
+// 	return (result);
+// }
 
 #include <string.h>
 #include <stdio.h>
+
 int	main(void)
 {
-	char	s1[6] = "mari";
-	char	s2[5] = "ana";
+	char*	s1[6] = "mari";
+	const char*	s2[5] = "ana";
 
-	// printf("%s", strcat(s1, s2));
-	printf("%s", ft_strcat(s1, s2));
+	printf("%s", strcat(&s1, s2));
+	// printf("%s", ft_strcat(s1, s2));
 	return (0);
 }
